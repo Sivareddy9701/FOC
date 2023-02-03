f226063c00f85e304f8ff71d09f7701d86619af1
Sivareddy9701 <122512238+Sivareddy9701@users.noreply.github.com>
2023-02-03 13:16:07 +0530

    Create 15.summing up the digits of an integer number .c

---
+++ 15.summing up the digits of an integer number .c
@@ -0,0 +1,13 @@
+:-#()
+{
+	int num;
+	
+	printf("input a number:");
+	scanf("%d",&num);
+	if (num == 0) 
+	    printf("%d is neither a positive, nor a negitive number\n",num);
+	else if (num>=0)
+	    printf("%d is a positive number\n",num);
+	else 
+	    printf("%d is a negitive number \n",num);
+}
